3b7ed30691053359094e494347bacd480fe9636d
moyashi170607 <takayaokada0607@gmail.com>
2025-05-18 04:52:24 +0900

    不要なファイルの削除

--- godot/script/UI/food_icon.gd
+++
@@ -1,9 +0,0 @@
-class_name FoodIcon
-
-extends Sprite2D
-
-@export var icon_number:int
-
-func _ready():
-	pass
-	
--- godot/script/UI/hp_icon.gd
+++
@@ -1,8 +0,0 @@
-class_name HPIcon
-
-extends Sprite2D
-
-@export var icon_number:int
-
-func _ready():
-	pass

1edd8aa24d9df24b4752f39ce2b71a278694346d
hinshiba <105423175+hinshiba@users.noreply.github.com>
2025-05-17 23:26:31 +0900

    初期化とclampによるエラー処理

--- godot/script/util/resource_t.gd
+++ godot/script/util/resource_t.gd
@@ -1,17 +1,22 @@
 class_name Resource_t
 
-var _resource: float = 1
+var _resource: float
 
 func _init():
-	_resource = 1
+	pass
+	
+static func newres(i: float = 0) -> Resource_t:
+	var instance = Resource_t.new()
+	instance._resource = i
+	return instance
 
 func getres() -> float:
 	return _resource
 
 func addres(a: float):
-	_resource = min(_resource + a, 1)
+	_resource = clampf(_resource + a, 0, 1)
 
 # リソースが0になるとtrueを返す
 func subres(s: float)-> bool:
-	_resource = max(_resource - s, 0)
+	_resource = clampf(_resource - s, 0, 1)
 	return _resource == 0.0

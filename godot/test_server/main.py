72e4f7eefb0be6f3a2a568f88272bc22c703a00e
hinshiba <105423175+hinshiba@users.noreply.github.com>
2025-05-18 11:35:36 +0900

    仮サーバのpick対応

--- godot/test_server/main.py
+++ godot/test_server/main.py
@@ -11,17 +11,29 @@ def allive():
 def reasoning():
     print(request.json)
     print(request.json["view_item"][49])
-    a = input()
-    b = input()
-    data = {
-        "id" : 1,
-        "next": {
-            "kind": "move",
-            "x": int(a),
-            "y": int(b)
+    exec_type = input("1: move; 2: pick > ")
+    if exec_type == "1":
+        a = input("x> ")
+        b = input("y> ")
+        data = {
+            "id" : 1,
+            "next": {
+                "kind": "move",
+                "x": int(a),
+                "y": int(b)
+            }
         }
-    }
-    return data
+        return data
+    elif exec_type == "2":
+        item = input("itemid> ")
+        data = {
+            "id" : 1,
+            "next": {
+                "kind": "pick",
+                "item": int(item)
+            }
+        }
+        return data
 
 if __name__ == '__main__':
     app.run(host='localhost', port=8000)
